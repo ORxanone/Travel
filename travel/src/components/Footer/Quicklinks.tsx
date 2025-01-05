640710e04754059e2cdb5fd633cad9a0f1ef3a06
Firuddin <Firuddinaliyev888@gmail.com>
2025-01-05 23:22:08 +0400

    footer end change

--- travel/src/components/Footer/Quicklinks.tsx
+++ travel/src/components/Footer/Quicklinks.tsx
@@ -1,31 +1,32 @@
 import React from 'react'
 import { Play } from 'lucide-react';
+import Link from 'next/link';
 const Quicklinks = () => {
     const listItems = [
-      "Flight",
-      "Tour",
-      "About",
-      "Terms of Service",
-      "Privacy Policy",
-      "Report an issue"
+        "Flight",
+        "Tour",
+        "About",
+        "Terms of Service",
+        "Privacy Policy",
+        "Report an issue"
     ];
-  
-    return (    
-      <div className="max-w-md  p-4">
-          <h2 className="text-2xl font-bold mb-5 text-[#D99528]">Quick Links</h2>
-        <ul>
-          {listItems.map((text, index) => (
-            <li 
-              key={index} 
-              className="flex items-center gap-3 p-1 rounded-lg hover:bg-gray-800 transition-colors"
-            >
-              <Play className="w-5 h-5 text-[#F9F9F9]" />
-              <span className="text-[#F9F9F9] ">{text}</span>
-            </li>
-          ))}
-        </ul>
-      </div>
+
+    return (
+        <div className="max-w-md  p-4">
+            <h2 className="text-2xl font-bold mb-5 text-[#D99528]">Quick Links</h2>
+            <ul>
+                {listItems.map((text, index) => (
+                    <li
+                        key={index}
+                        className="flex items-center gap-3 p-1 rounded-lg hover:bg-gray-800 transition-colors"
+                    >
+                        <Play className="w-5 h-5 text-[#F9F9F9]" />
+                        <Link className="text-[#F9F9F9] " href={"#"}>{text}</Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
-  };
+};
 
 export default Quicklinks
--- travel/src/components/Footer/Tour.tsx
+++ travel/src/components/Footer/Tour.tsx
@@ -4,14 +4,16 @@ import Image from 'next/image'
 import Quicklinks from './Quicklinks'
 import Contact from './Contact'
 import Copyright from './Copyright'
+import Link from 'next/link'
+
 const Footer = () => {
   return (
     <div className='bg-[#115779] h-[463] '>
       <div className='flex justify-evenly pt-10'>
         <div>
           <Image src="/Footerlogo.png" alt="Footerlogo" width={180.44} height={48} />
-          <p className='mt-6 w-[430] text-[#E3E3E3]'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standa</p>
-          <a href="#" className=' text-[#D99528] mt-9 block'>Read More</a>
+          <p className='mt-6 text-[#E3E3E3]'>Lorem Ipsum is simply dummy text of the printing <br /> and typesetting industry. Lorem Ipsum has been <br /> the industry&aposs standa</p>
+          <Link href="#" className=' text-[#D99528] mt-9 block'>Read More</Link>
           <hr className='w-[373] mt-3 mb-3' />
           <Footersosial/>
         </div>
